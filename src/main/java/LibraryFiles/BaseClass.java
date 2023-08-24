347d81ad5107b3d1296f021fb35a20553e7d498c
❤Chinmay❤ <Chinmay@DESKTOP-S4B5J1M.mshome.net>
2023-08-24 20:18:08 +0530

    [24/08/23] Mvn Run Through CMD

--- src/main/java/LibraryFiles/BaseClass.java
+++ src/main/java/LibraryFiles/BaseClass.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseClass 
 {
@@ -13,8 +14,12 @@ public class BaseClass
 	
 	public void initializeBrowser() throws IOException 
 	{	
-		driver=new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		
+		driver=new ChromeDriver(options);
 		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
 		driver.get(UtilityClass.getPFData("URL"));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 	}
--- src/main/java/Module1_Login/PBMobNumPage.java
+++ src/main/java/Module1_Login/PBMobNumPage.java
@@ -1,6 +1,7 @@
 package Module1_Login;
 //pom class 2
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,12 +11,25 @@ public class PBMobNumPage
 {
 	@FindBy(xpath="(//input[@id='central-login-module-sign-mobile'])[2]") private WebElement mobNum;
 	@FindBy(xpath="(//span[text()='Sign in with Password'])[2]") private WebElement signInWithPwd;
+//	@FindBy(xpath="(//img[contains(@class,'ce')])[2]") private WebElement country;
 	
+//	private WebDriver driver;
+ //  private WebDriverWait wait;
 	
 	public PBMobNumPage(WebDriver driver)
 	{
+	//	this.driver = driver;
+	//	this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		
 		PageFactory.initElements(driver, this);
 	}
+	                 
+//	public void selectCountryCode() throws InterruptedException {
+//	((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);",country);	
+//	//wait.until(ExpectedConditions.elementToBeClickable(country));
+//	Thread.sleep(2000);
+//	country.click();
+//	}
 	
 	public void inpPBMobNumPageMobNum(String mobileNum)
 	{
